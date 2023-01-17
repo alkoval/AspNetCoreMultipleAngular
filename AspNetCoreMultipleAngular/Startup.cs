73aada99cc30257e5103b3898f04ac881f6d8b6f
alkoval <koval.alk@yandex.ru>
2023-01-17 16:57:46 +0500

    Removed client apps

--- AspNetCoreMultipleAngular/Startup.cs
+++ AspNetCoreMultipleAngular/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
